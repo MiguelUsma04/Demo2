35e9821d16332cb36391d45900cd26c330f1942b
Miguel Usma <usmamiguel04@gmail.com>
2024-04-12 17:29:11 -0500

    Employee Class and Program Changes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿using MyLibrary;
 
-int i = 5;
-string name = "Hello";
+Secretary Maria = new Secretary();
+Maria.Name = "Maria";
+Manager Pedro = new Manager();
+Pedro.Name = "Pedro";
+Developer Miguel = new Developer();
 
-Product Leche;
-Leche = new Product(1, "Leche entera", 19, 100);
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Pedro);
+WriteEmployeeInfo(Miguel);
+
+void WriteEmployeeInfo(Employee employee)
+{
+    Console.WriteLine($"{employee.Name}: {employee.CalculateMonthlyPayment()}");
+}
